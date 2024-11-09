9ebcdffdd75fe0984e9018687b80e26d9e56eba3
HikaruPractice <119712253+HikaruPractice@users.noreply.github.com>
2024-11-10 01:24:36 +0900

    Add files via upload

--- tenkey.js
+++ tenkey.js
@@ -16,7 +16,7 @@ let SymbolNames = [
 ]
 window.addEventListener('DOMContentLoaded', function() {
     //交換した数を入力するためのテンキー
-    createTenkey('change');
+    createTenkey2('change');
     createTenkey('got');
 })
 function createTenkey(name){
@@ -31,6 +31,21 @@ function createTenkey(name){
             button.id = 'button_' + SymbolNames[i][j];
             button.textContent = Symbols[i][j];
             td_element_buttons.appendChild(button);
+            tr_element_buttons.appendChild(td_element_buttons);
+        }
+        target.appendChild(tr_element_buttons);
+    }
+}
+function createTenkey2(name){
+    target=document.getElementsByClassName('tenkey_'+name)[0].getElementsByTagName('tbody')[0]
+    for (let i = 0; i < Symbols.length; i++) {
+        tr_element_buttons = document.createElement('tr');
+        for (let j = 0; j < Symbols[0].length; j++) {
+            td_element_buttons = document.createElement('td');
+            td_element_buttons.id = name+'_td_' + SymbolNames[i][j];
+            td_element_buttons.onclick = event => pushButton(name,Symbols[i][j]);
+            td_element_buttons.id = 'button_' + SymbolNames[i][j];
+            td_element_buttons.textContent = Symbols[i][j];
             tr_element_buttons.appendChild(td_element_buttons);
         }
         target.appendChild(tr_element_buttons);
