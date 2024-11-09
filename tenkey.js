ec7a9ce0817b9a45e0b752bb0bd28dd407de933c
HikaruPractice <119712253+HikaruPractice@users.noreply.github.com>
2024-11-10 02:12:04 +0900

    Add files via upload

--- tenkey.js
+++ tenkey.js
@@ -17,29 +17,11 @@ let SymbolNames = [
 ]
 window.addEventListener('DOMContentLoaded', function() {
     //交換した数を入力するためのテンキー
-    createTenkey2('change');
+    createTenkey('change');
     createTenkey('got');
 })
+
 function createTenkey(name){
-    target=document.getElementsByClassName('tenkey_'+name)[0].getElementsByTagName('tbody')[0]
-    for (let i = 0; i < Symbols.length; i++) {
-        tr_element_buttons = document.createElement('tr');
-        for (let j = 0; j < Symbols[0].length; j++) {
-            td_element_buttons = document.createElement('td');
-            td_element_buttons.id = name+'_td_' + SymbolNames[i][j];
-            button = document.createElement('button');
-            button.setAttribute(_click,
-                `pushButton('${name}','${Symbols[i][j]}')`
-            );
-            button.id = 'button_' + SymbolNames[i][j];
-            button.textContent = Symbols[i][j];
-            td_element_buttons.appendChild(button);
-            tr_element_buttons.appendChild(td_element_buttons);
-        }
-        target.appendChild(tr_element_buttons);
-    }
-}
-function createTenkey2(name){
     target=document.getElementsByClassName('tenkey_'+name)[0].getElementsByTagName('tbody')[0]
     for (let i = 0; i < Symbols.length; i++) {
         tr_element_buttons = document.createElement('tr');
